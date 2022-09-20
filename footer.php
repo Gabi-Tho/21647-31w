59e7fc3216af9c30d9bb14c295eabeed08aedbd1
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-09-20 15:28:02 -0400

    ajout simple footer.php

--- footer.php
+++ footer.php
@@ -11,20 +11,8 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscore' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'underscore' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscore' ), 'underscore', '<a href="http://underscores.me/">Gabi</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer>
+		<p>mon pied de page bebe</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
