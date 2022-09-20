a4b9d12b5f1e2c4fa6965b748e4cc908ac0d6428
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-09-20 15:19:52 -0400

    ajoute header.php

--- functions.php
+++ functions.php
@@ -62,11 +62,13 @@ function underscore_scripts() {
 	// 				);
 
 	wp_enqueue_style('underscore-style',
-					 get_template_directory_uri() . '/css/style.css',
+					 get_template_directory_uri() . '/style.css',
 					 array(), 
-					 filemtime(get_template_directory() . '/css/style.css'),
+					 filemtime(get_template_directory() . '/style.css'),
 					 false
 					);
 	
 }
 add_action( 'wp_enqueue_scripts', 'underscore_scripts' );
+
+
