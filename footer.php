0a86ca889bad0c7aeef9cf664b7d67c45a655d09
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-10-28 11:23:24 -0400

    ajoute sidebar-footer-3 et 4

--- footer.php
+++ footer.php
@@ -14,8 +14,8 @@
 <footer class="site__footer">
 		<div><?php get_sidebar( 'footer-1' ); ?></div>
 		<div><?php get_sidebar( 'footer-2' ); ?></div>
-		<div>Gabi</div>
-		<div>Gabi</div>
+		<div><?php get_sidebar( 'footer-3' ); ?></div>
+		<div><?php get_sidebar( 'footer-4' ); ?></div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
