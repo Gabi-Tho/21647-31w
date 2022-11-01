1bf5a6d6280b063509c6324a840b21d02a1c41ce
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-11-01 15:12:58 -0400

    ajoute the_field dans front-page.php

--- front-page.php
+++ front-page.php
@@ -27,7 +27,9 @@ get_header();
             while ( have_posts() ) :
 				the_post();?>
                 <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+                <h2><?php the_field('heading'); ?></h2>
                 <?php the_content(null,true); ?>
+
                 <?php endwhile; ?>
         <?php endif; ?>
     </main>    
