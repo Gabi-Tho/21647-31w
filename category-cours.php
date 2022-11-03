fe6f37e9f684f48e6d3e69a0f6c9d2ccf1c32ff1
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-11-03 12:27:47 -0400

    edit h2 dans category cours

--- category-cours.php
+++ category-cours.php
@@ -28,9 +28,9 @@ get_header();
 				the_post();?>
                 <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
                 <h2>Durée de cours:<?php the_field('duree'); ?></h2>
-                <h2>Durée de cours:<?php the_field('email'); ?></h2>
-                <h2>Durée de cours:<?php the_field('radio'); ?></h2>
-                <h2>Durée de cours:<?php the_field('valeurs'); ?></h2>
+                <h2>courriel:<?php the_field('email'); ?></h2>
+                <h2>evaluation de prof: <?php the_field('radio'); ?></h2>
+                <h2>moyenne:<?php the_field('valeurs'); ?></h2>
                 <?php the_content(null,true); ?>
 
                 <?php endwhile; ?>
